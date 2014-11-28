5c3f7f56912aaddb881d568ae3346b4e8f8ca235
Andreas Precht Poulsen <andreas.p.poulsen@gmail.com>
2014-11-28 12:07:35 +0100

    added gettopgenres with streaming

--- AlgorithmProject/src/com/cc/logic/MisraGriesLogic.java
+++ AlgorithmProject/src/com/cc/logic/MisraGriesLogic.java
@@ -1,5 +1,7 @@
 package com.cc.logic;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -24,9 +26,32 @@ public class MisraGriesLogic {
 				}
 			}
 		}
-		
 		return getHighestValueKeysOfMap(map, buckets);
-		
+	}
+	
+	public static String[] getTopGenres(BufferedReader stream, double percentage) {
+		int buckets = (int) (1.0/percentage) + 1;
+		// Instantiate the map.
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		String genre;
+		try{
+			while((genre=stream.readLine()) != null) {
+				if(map.containsKey(genre)) {
+					// Increase bucket count if it already exists in map.
+					map.put(genre, map.get(genre)+1);
+				} else {
+					if(map.size() < buckets) {
+						map.put(genre, 1);
+					} else {
+						shrinkMap(map, buckets);
+						map.put(genre, 1);
+					}
+				}
+			}
+		} catch(IOException exn) {
+			exn.printStackTrace();
+		}
+		return getHighestValueKeysOfMap(map, buckets);
 	}
 	
 	private static void shrinkMap(Map<String, Integer> map, int buckets) {
