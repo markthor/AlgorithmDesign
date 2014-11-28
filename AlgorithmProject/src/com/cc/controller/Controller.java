7bf7c6dda54b6c698070e07e60782000c5349b2d
Mark <markthorh@gmail.com>
2014-11-28 11:00:02 +0100

    added additional code

--- AlgorithmProject/src/com/cc/controller/Controller.java
+++ AlgorithmProject/src/com/cc/controller/Controller.java
@@ -1,5 +1,43 @@
 package com.cc.controller;
 
-public class Controller {
+import java.util.ArrayList;
+import java.util.List;
 
+import com.cc.logic.StreamingLogic;
+
+public class Controller {
+	public static void main(String args[]) {
+		List<String> testList = new ArrayList<String>();
+		testList.add("Porn");
+		testList.add("Porn");
+		testList.add("Porn");
+		testList.add("Comedy");
+		testList.add("Romance");
+		testList.add("Adventure");
+		testList.add("Thriller");
+		testList.add("Horror");
+		testList.add("Porn");
+		testList.add("Comedy");
+		testList.add("Adventure");
+		testList.add("Thriller");
+		testList.add("Horror");
+		testList.add("Porn");
+		testList.add("Comedy");
+		testList.add("Thriller");
+		testList.add("Thriller");
+		testList.add("Thriller");
+		testList.add("Thriller");
+		testList.add("Romance");
+		testList.add("Romance");
+		testList.add("Romance");
+		testList.add("Comedy");
+		testList.add("Dungeon");
+
+		
+		String[] result = StreamingLogic.getTopGenres(testList.iterator(), 0.15);
+		for(int i = 0; i < result.length; i++) {
+			System.out.println(result[i]);
+		}
+
+	}
 }
--- AlgorithmProject/src/com/cc/logic/StreamingLogic.java
+++ AlgorithmProject/src/com/cc/logic/StreamingLogic.java
@@ -5,38 +5,36 @@ import java.util.Iterator;
 import java.util.Map;
 
 public class StreamingLogic {
-	private static final int NUMBER_OF_GENRES = 4;
-	private static final int NUMBER_OF_BUCKETS = NUMBER_OF_GENRES + 1;
-	private static StreamingLogic instance = new StreamingLogic();
 	
-	public int[] getTopGenres(Iterator<Integer> stream) {
+	public static String[] getTopGenres(Iterator<String> stream, double percentage) {
+		int buckets = (int) (1.0/percentage) + 1;
 		// Instantiate the map.
-		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		Map<String, Integer> map = new HashMap<String, Integer>();
 		while(stream.hasNext()) {
-			Integer id = stream.next();
-			if(map.containsKey(id)) {
+			String genre = stream.next();
+			if(map.containsKey(genre)) {
 				// Increase bucket count if it already exists in map.
-				map.put(id, map.get(id)+1);
+				map.put(genre, map.get(genre)+1);
 			} else {
-				if(map.size() < NUMBER_OF_BUCKETS) {
+				if(map.size() < buckets) {
 					map.put(stream.next(), 1);
 				} else {
-					shrinkMap(map);
+					shrinkMap(map, buckets);
 					map.put(stream.next(), 1);
 				}
 			}
 		}
 		
-		return getHighestValueKeysOfMap(map);
+		return getHighestValueKeysOfMap(map, buckets);
 		
 	}
 	
-	private void shrinkMap(Map<Integer, Integer> map) {
-		Iterator it = map.entrySet().iterator();
+	private static void shrinkMap(Map<String, Integer> map, int buckets) {
+		Iterator<Map.Entry<String, Integer>> it = map.entrySet().iterator();
 		// Iterate through the keys as long as the map is full.
-		while(map.size() == NUMBER_OF_BUCKETS) {
+		while(map.size() == buckets) {
 		    while (it.hasNext()) {
-		        Map.Entry<Integer, Integer> pair = (Map.Entry<Integer, Integer>)it.next();
+		        Map.Entry<String, Integer> pair = it.next();
 		        // Remove key if it gets reduced to zero.
 		        if(pair.getValue().equals(1)) {
 		        	map.remove(pair.getKey());
@@ -48,29 +46,26 @@ public class StreamingLogic {
 		}
 	}
 	
-	private int[] getHighestValueKeysOfMap(Map<Integer, Integer> map) {
-		Iterator it = map.entrySet().iterator();
-		int[] result = new int[NUMBER_OF_GENRES];
+	private static String[] getHighestValueKeysOfMap(Map<String, Integer> map, int buckets) {
+		Iterator<Map.Entry<String, Integer>> it = map.entrySet().iterator();
+		String[] result = new String[buckets];
 		int min = Integer.MAX_VALUE;
 		int minIndex = 0;
 		int i = 0;
 		while(it.hasNext()) {
-			Map.Entry<Integer, Integer> pair = (Map.Entry<Integer, Integer>)it.next();
+			Map.Entry<String, Integer> pair = it.next();
 			if(pair.getValue() < min) {
 				min = pair.getValue();
 				minIndex = i;
 			}
-			if(i < NUMBER_OF_GENRES) {
+			if(i < buckets - 1) {
 				result[i] = pair.getKey();
 			} else {
 				if(!(pair.getValue() == min))
 					result[minIndex] = pair.getKey();
 			}
+			i++;
 		}
 		return result;
-	}
-	
-	private static StreamingLogic getInstance() {
-		return instance;
 	}
 }
