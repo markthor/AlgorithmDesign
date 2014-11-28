0f4e3c8e606b14e3db129a837a2cbdb9b49932d8
Andreas Precht Poulsen <andreas.p.poulsen@gmail.com>
2014-11-28 10:54:34 +0100

    added script to clean genres

--- data/data.py
+++ data/data.py
@@ -6,14 +6,14 @@ pattern = re.compile("LOCK TABLES `([\w]+)` WRITE;")
 
 # Loop through every line in the input file
 outputfile = 0
-importfile = codecs.open("import.sql", "w+", encoding="utf-8");
-with codecs.open("imdb-r.txt", "r", encoding="utf-8") as datafile:
+importfile = codecs.open("data_files/import.sql", "w+", encoding="utf-8");
+with codecs.open("data_files/imdb-r.txt", "r", encoding="utf-8") as datafile:
 	for line in datafile:
 		if pattern.match(line):
 			# If the pattern matches, it is a new database table
 
 			filename = pattern.match(line).group(1)
-			outputfile = codecs.open(filename + ".txt", "w+", encoding="utf-8")
+			outputfile = codecs.open("data_files/" + filename + ".txt", "w+", encoding="utf-8")
 			importfile.write("LOAD DATA LOCAL INFILE '" + filename
 							+ ".txt' INTO TABLE " + filename + ";\n")
 		else:
